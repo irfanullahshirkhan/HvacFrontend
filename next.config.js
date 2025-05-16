0f0e56153edfbcf9c65cc2af3a3bbef81a1d9898
irfanullahshirkhan <irfanullahshirkhan@gmail.com>
2025-05-17 00:41:49 +0430

    fixed next.config issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export", // Required for static export
   images: {
+    unoptimized: true, // Required when using next/image with static export
     domains: [
       "images.unsplash.com",
       "randomuser.me",
