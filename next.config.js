cc292204df751d2b9262fd67b9f6ee0c51ea54ed
irfanullahshirkhan <irfanullahshirkhan@gmail.com>
2025-05-18 17:59:55 +0430

    added online images instead of local in services

--- next.config.js
+++ next.config.js
@@ -5,8 +5,28 @@ const nextConfig = {
       "images.unsplash.com",
       "randomuser.me",
       "upload.wikimedia.org",
+      "img.freepik.com",
+      "cdn.pixabay.com",
+      "images.pexels.com"
     ],
-  }
+  },
+  // Enable Turbopack optimizations
+  experimental: {
+    turbo: {
+      rules: {
+        // Configure Turbopack rules
+        '*.{js,jsx,ts,tsx}': ['swc'],
+      },
+    },
+    // Enable other performance optimizations
+    optimizeCss: true,
+    optimizePackageImports: ['@mui/icons-material', '@mui/material', '@emotion/styled', '@emotion/react'],
+  },
+  // Production optimizations
+  swcMinify: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  compress: true,
 }
 
 module.exports = nextConfig
